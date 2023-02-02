a69365dda3ed474dffcfc660d4606dce161a35bc
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-02 13:37:39 +0000

    Add Sidebar function

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+// 1. Create functional component (as a regular function)
+ function Sidebar() {
+     return <div>
+        <a href="">My Photos</a>
+        <a href="">My Illustrations</a>
+        <a href="">My Paintings</a>
+     </div>
+}
+
+// 2. Export functional component
+export default Sidebar; 
