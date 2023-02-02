5352c24c4b12d8742b91c9c344f0bf4a17ca8f01
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-02 13:43:27 +0000

    Add styling to Sidebar function

--- my-app/src/components/Sidebar.js
+++ my-app/src/components/Sidebar.js
@@ -1,9 +1,10 @@
 import React from "react";
+import css from "./css/Sidebar.module.css"
 
 
 // 1. Create functional component (as a regular function)
  function Sidebar() {
-     return <div>
+     return <div className = {css.sidebar}>
         <a href="">My Photos</a>
         <a href="">My Illustrations</a>
         <a href="">My Paintings</a>
