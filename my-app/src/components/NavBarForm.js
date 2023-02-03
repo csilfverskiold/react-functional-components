78f9f47682eb822e73c6de31b58242dfeacce3d2
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-03 22:59:46 +0000

    Add NavBarForm

---
+++ my-app/src/components/NavBarForm.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import css from "./css/NavBarForm.module.css"
+
+export class NavBarForm extends Component {
+
+    constructor(props) {
+        super(props)
+        
+        this.state = {
+            isLoggedIn: true,
+        }
+    }
+    handleClick = () => {
+        this.setState((prevState) => ({
+            isLoggedIn: prevState.isLoggedIn ? false: true
+        }),  () => console.log(this.state.isLoggedIn))
+    }
+
+    render() {
+        return (
+            <div className={css.NavBar}>
+                <h1>My Gallery</h1>
+                {
+                    this.state.isLoggedIn ?
+                        <button onClick={() => this.handleClick()}>Login</button>
+                        :
+                        <form>
+                            <label htmlFor="username">Username:</label>
+                            <input placeholder="username" id="username" />
+
+                            <label htmlFor="password">Password:</label>
+                            <input placeholder="password" id="password" />
+                            <button onClick={() => this.handleClick()}>Submit</button>
+                        </form>
+                }
+            </div>
+        )
+    }
+}
+
+export default NavBarForm
--- my-app/src/components/NavBarSimple.js
+++ my-app/src/components/NavBarSimple.js
@@ -1,24 +1,6 @@
 import React from "react";
 import css from "./css/NavBarSimple.module.css"
 
-// 1.0 Create class component (as a regular class) DONE
-// 1.1 Extend class component through React ("extends React.Component") DONE
-// 1.2 Define a render method and return DONE
-// 1.2 Pass in greeting prop in class (don't forget "this"!)
-//
-// 1.3 Add state to a class component: First define the constructor,  
-//     call super, then add the initial state object inside the constructor.
-//     Add constructor, a special method that is always called when the class is first instantiated.
-//     
-// 1.4.0 Add event handler to class component, example: Add onClick attribute, define handleClick method in
-//       class component, call setState method containing objects in handleClick method, 
-//       add callback function "() => {" after setState method to render properly in console. 
-//
-// 1.4.1 Add ternary condition to event handler, to change from "Hello! / Exit" to "Goodbye! / Enter"
-//       with === and ? :
-
-// 1.4.2 Continue to add third rule of updating the state, follow supplied the "Updating the State - Cheat Sheet"
-//
 class NavBarSimple extends React.Component {
 
     constructor(props) {
