b4ee46955cc541b9591fa98fd2edc9695afa4c02
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-07 12:22:18 +0000

    Add component lifecycle method in Content.js

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { savedPosts } from '../posts.json'
 import css from './css/Content.module.css'
 import PostItem from './PostItem'
+import Loader from './Loader'
 
 
 export class Content extends Component {
@@ -9,8 +10,18 @@ export class Content extends Component {
         super(props)
 
         this.state = {
+            isLoaded: false
         }
     }
+
+componentDidMount() {
+    setTimeout(() => {
+        this.setState({
+            isLoaded: true
+        })
+    }, 2000)
+}
+
     render() {
         return (
             <div className={css.Content}>
@@ -18,7 +29,7 @@ export class Content extends Component {
                     <h1>My Photos</h1>
                 </div>
                 <div className={css.SearchResults}>
-                    <PostItem savedPosts={savedPosts} />
+                    {this.state.isLoaded ? <PostItem savedPosts={savedPosts} /> : <Loader />}
                 </div>
             </div>
         )
