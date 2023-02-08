8d5c62ff7656d1a73df1f0562173617f29e7197d
csilfverskiold <christian.silfverskiold@gmail.com>
2023-02-08 08:45:18 +0000

    Add filter form search functionality to content

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -10,26 +10,45 @@ export class Content extends Component {
         super(props)
 
         this.state = {
-            isLoaded: false
+            isLoaded: false,
+            posts: [],
         }
     }
 
 componentDidMount() {
     setTimeout(() => {
         this.setState({
-            isLoaded: true
+            isLoaded: true,
+            posts: savedPosts,
         })
     }, 2000)
 }
+
+handleChange = (event) => {
+    const name = event.target.value.toLowerCase()
+    console.log(name)
+    const filteredPosts = savedPosts.filter((post) => {
+      return post.name.toLowerCase().includes(name)
+    })
+    this.setState({
+      posts: filteredPosts
+    })
+  }
+  
 
     render() {
         return (
             <div className={css.Content}>
                 <div className={css.TitleBar}>
                     <h1>My Photos</h1>
+                    <form>
+                        <label htmlFor = "searchInput">Search</label>
+                        <input type="search" id="searchInput" placeholder="By Author" onChange={(event) => this.handleChange(event)}/>
+                        <h4>posts found: {this.state.posts.length}</h4>
+                    </form>
                 </div>
                 <div className={css.SearchResults}>
-                    {this.state.isLoaded ? <PostItem savedPosts={savedPosts} /> : <Loader />}
+                    {this.state.isLoaded ? <PostItem savedPosts={this.state.posts} /> : <Loader />}
                 </div>
             </div>
         )
